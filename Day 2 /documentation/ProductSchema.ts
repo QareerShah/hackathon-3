2ede13a61bced63ea6c8e8b0f138419822abe843
QareerShah <160770415+QareerShah@users.noreply.github.com>
2025-01-17 01:36:59 +0500

    Create ProductSchema.ts

---
+++ Day 2 /documentation/ProductSchema.ts
@@ -0,0 +1,9 @@
+export default {
+  name: 'product',
+  type: 'document',
+  fields: [
+    { name: 'name', type: 'string', title: 'Product Name' },
+    { name: 'price', type: 'number', title: 'Price' },
+    { name: 'stock', type: 'number', title: 'Stock Level' }
+  ]
+};
