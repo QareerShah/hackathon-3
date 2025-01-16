b852bbb9b84130b2a4f5c3314a4d818217b4d7a7
QareerShah <160770415+QareerShah@users.noreply.github.com>
2025-01-17 02:04:08 +0500

    Update OrderSchema.ts

--- Day 2 /documentation/OrderSchema.ts
+++ Day 2 /documentation/OrderSchema.ts
@@ -1,9 +1,44 @@
-export default {
+import { defineType } from 'sanity';
+
+export const orderSchema = defineType({
   name: 'order',
+  title: 'Order',
   type: 'document',
   fields: [
-    { name: 'customer', type: 'string', title: 'Customer Name' },
-    { name: 'productDetails', type: 'array', title: 'Product Details' },
-    { name: 'paymentStatus', type: 'string', title: 'Payment Status' }
-  ]
-};
+    {
+      name: 'productId',
+      title: 'Product ID',
+      type: 'reference',
+      to: [{ type: 'product' }],
+    },
+    {
+      name: 'customerId',
+      title: 'Customer ID',
+      type: 'reference',
+      to: [{ type: 'customer' }],
+    },
+    {
+      name: 'orderDate',
+      title: 'Order Date',
+      type: 'datetime',
+    },
+    {
+      name: 'quantity',
+      title: 'Quantity',
+      type: 'number',
+    },
+    {
+      name: 'totalPrice',
+      title: 'Total Price',
+      type: 'number',
+    },
+    {
+      name: 'status',
+      title: 'Status',
+      type: 'string',
+      options: {
+        list: ['Pending', 'Delivered', 'Cancelled'],
+      },
+    },
+  ],
+});
