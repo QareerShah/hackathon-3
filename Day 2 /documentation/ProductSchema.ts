41ff3dfe294849d931b5fef7c45b5069256b305c
QareerShah <160770415+QareerShah@users.noreply.github.com>
2025-01-17 02:03:04 +0500

    Update ProductSchema.ts

--- Day 2 /documentation/ProductSchema.ts
+++ Day 2 /documentation/ProductSchema.ts
@@ -1,9 +1,50 @@
-export default {
+import { defineType } from 'sanity';
+
+export const productSchema = defineType({
   name: 'product',
+  title: 'Product',
   type: 'document',
   fields: [
-    { name: 'name', type: 'string', title: 'Product Name' },
-    { name: 'price', type: 'number', title: 'Price' },
-    { name: 'stock', type: 'number', title: 'Stock Level' }
-  ]
-};
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+    },
+    {
+      name: 'stock',
+      title: 'Stock',
+      type: 'number',
+    },
+    {
+      name: 'category',
+      title: 'Category',
+      type: 'string',
+      options: {
+        list: ['Shoes', 'Tops', 'Winter Collection', 'Accessories'],
+      },
+    },
+    {
+      name: 'size',
+      title: 'Size',
+      type: 'string',
+    },
+    {
+      name: 'gender',
+      title: 'Gender',
+      type: 'string',
+      options: {
+        list: ['Men', 'Women', 'Kids'],
+      },
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    },
+  ],
+});
